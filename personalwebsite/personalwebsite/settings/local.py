e621f0cf145ed0fef25bf886de204c05ff4911a4
Kevin <kevin@superb-server.c.superb-deployer.internal>
2018-02-01 17:40:03 +0000

    update

--- personalwebsite/personalwebsite/settings/local.py
+++ personalwebsite/personalwebsite/settings/local.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'artistpainting',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'personalwebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
